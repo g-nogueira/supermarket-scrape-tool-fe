9b46a7dab331d314f4c0d3f8b6999801097d74dc
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2023-10-21 17:21:27 +0100

    feat: add image, link, and price unit to ProductList

--- src/routes/products/ProductList.svelte
+++ src/routes/products/ProductList.svelte
@@ -1,13 +1,14 @@
 <!-- ProductList.svelte -->
 <script lang="ts">
-	export let products: Array<{ id: number; name: string; price: number }>;
+	export let products: Array<{ id: number; name: string; price: number; priceUnit: string; source: string; imageUrl: string}>;
 </script>
 
 {#each products as product (product.id)}
 	<div class="product">
 		<!-- Display product information here -->
-		<h3>{product.name}</h3>
-		<p>Price: ${product.price.toFixed(2)}</p>
+		<img src="{product.imageUrl}" width="500px" alt="Image of {product.name}"/>
+		<h3><a href={`/product/${product.id}`}>{product.name}</a></h3>
+		<p>Price: ${product.price.toFixed(2)} {product.priceUnit} ({product.source})</p>
 		<!-- Add more details as needed -->
 	</div>
 {/each}
