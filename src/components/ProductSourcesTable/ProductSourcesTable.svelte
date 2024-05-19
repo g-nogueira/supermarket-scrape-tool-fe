11b45cd118e5c562f1b315284e869fd00855c873
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-05-19 22:49:56 +0100

    refactor: Improve ProductSourcesTable component

--- src/components/ProductSourcesTable/ProductSourcesTable.svelte
+++ src/components/ProductSourcesTable/ProductSourcesTable.svelte
@@ -4,95 +4,92 @@
 		TableBody,
 		TableBodyCell,
 		TableBodyRow,
-		TableHead,
 		TableHeadCell
 	} from 'flowbite-svelte';
 
-    import type { ProductDetail, ProductPriceHistory } from '../../types/ProductDetail';
-    import { reduce } from 'fp-ts/Array';
-    import { pipe } from 'fp-ts/function';
-    import { type Ord, max } from 'fp-ts/Ord';
-    import { contramap } from 'fp-ts/Ord';
-    import { isNone } from 'fp-ts/Option';
+	import type { ProductDetail, ProductPriceHistory } from '../../types/ProductDetail';
+	import { map, reduce } from 'fp-ts/Array';
+	import { pipe } from 'fp-ts/function';
+	import { type Ord, max } from 'fp-ts/Ord';
+	import { contramap } from 'fp-ts/Ord';
+	import { sourceName } from '../../types/SourceName';
 
-    type PriceHistoryBySourceMatrix = { [key: string]: ProductPriceHistory };
-    type PriceHistoryBySource = { name: string } & ProductPriceHistory;
+	type PriceHistoryBySourceMatrix = { [key: string]: ProductPriceHistory };
+	type PriceHistoryBySource = {
+		name: string;
+		priceString: string;
+		dateString: string;
+	} & ProductPriceHistory;
 
+	export let product: ProductDetail;
 
-// export enum ProductPriceUnit {
-//     Kg,
-//     Unit,
-// }
+	let currentLanguage = navigator.language;
 
-// export type ProductSource = {
-//     productId: string;
-//     name: SourceName;
-//     productUrl: string;
-//     productImageUrl: string;
-// };
+	const dateOrd: Ord<Date> = {
+		equals: (x, y) => x.getTime() === y.getTime(),
+		compare: (x, y) => (x.getTime() === y.getTime() ? 0 : x.getTime() > y.getTime() ? 1 : -1)
+	};
 
-// export type ProductPriceHistory = {
-//     date: Date;
-//     price: number;
-//     priceUnit: ProductPriceUnit;
-//     source: ProductSource;
-// };
+	const priceHistoryOrd: Ord<ProductPriceHistory> = contramap(
+		(priceHistory: ProductPriceHistory) => priceHistory.date
+	)(dateOrd);
 
-// export type ProductDetail = {
-//     id: string;
-//     name: string;
-//     brand: string;
-//     priceHistory: ProductPriceHistory[];
-//     sources: ProductSource[];
-//     ean: string;
-// };
+	const mkRelativeDate = (date: Date) => {
+		const rtf = new Intl.RelativeTimeFormat(currentLanguage, { numeric: 'auto' });
+		const diff = date.getTime() - new Date().getTime();
+		const diffInDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+		return rtf.format(diffInDays, 'day');
+	};
 
+	const mkPriceHistoryBySource = ([name, priceHistory]: [string, ProductPriceHistory]) => {
+		return {
+			name,
+			priceString: `${priceHistory.price} €`,
+			dateString: mkRelativeDate(priceHistory.date),
+			...priceHistory
+		} as PriceHistoryBySource;
+	};
 
-    export let product: ProductDetail;
+	const priceHistoryBySource = pipe(
+		product.priceHistory,
+		reduce({}, (acc: PriceHistoryBySourceMatrix, priceHistory) => {
+			const sourceNameStr = pipe(priceHistory.source.name, sourceName.toString);
 
-    const dateOrd: Ord<Date> = {
-        equals: (x, y) => x.getTime() === y.getTime(),
-        compare: (x, y) => (x.getTime() === y.getTime() ? 0 : x.getTime() > y.getTime() ? 1 : -1)
-    };
+			const currentEntry = acc[sourceNameStr];
 
-    const priceHistoryOrd: Ord<ProductPriceHistory> = contramap((priceHistory: ProductPriceHistory) => priceHistory.date)(dateOrd);
-
-
-    // group PriceHistory by source and get only the most recent price for each source
-    const priceHistoryBySource = pipe(
-        product.priceHistory,
-        reduce({}, (acc: PriceHistoryBySourceMatrix, priceHistory) => {
-            const currentEntry = acc[priceHistory.source.name];
-            if (!currentEntry || max(priceHistoryOrd)(currentEntry, priceHistory) === priceHistory) {
-                acc[priceHistory.source.name] = priceHistory;
-            }
-            return acc;
-        }),
-        Object.entries,
-        (entries) => entries.map(([name, priceHistory]) => ({ name, ...priceHistory } as PriceHistoryBySource))
-    );
-
+			if (!currentEntry || max(priceHistoryOrd)(currentEntry, priceHistory) === priceHistory) {
+				acc[sourceNameStr] = priceHistory;
+			}
+			return acc;
+		}),
+		Object.entries,
+		map(mkPriceHistoryBySource)
+	);
 </script>
 
 <Table>
 	<TableBody tableBodyClass="divide-y">
-        <!-- List of product sources -->
-        {#each priceHistoryBySource as source}
-            <TableBodyRow>
-                <TableBodyCell>
-                    <img src={source.source.productImageUrl} alt={source.name} class="w-16 max-w-full max-h-full" />
-                </TableBodyCell>
-                <TableBodyCell>
-                    <span class="px-6 py-4 font-semibold text-gray-900 dark:text-white">{source.name}</span>
-                </TableBodyCell>
-                <TableBodyCell>{source.price}</TableBodyCell>
-                <TableBodyCell>{source.date}</TableBodyCell>
-                <TableHeadCell>
-                    <a href={source.source.productUrl} target="_blank" rel="noopener noreferrer">
-                        <span class="sr-only">See</span>
-                    </a>
-                </TableHeadCell>
-            </TableBodyRow>
-        {/each}
+		<!-- List of product sources -->
+		{#each priceHistoryBySource as source}
+			<TableBodyRow>
+				<TableBodyCell>
+					<img
+						src={source.source.productImageUrl}
+						alt={source.name}
+						class="max-h-full w-16 max-w-full"
+					/>
+				</TableBodyCell>
+				<TableBodyCell>
+					<span class="px-6 py-4 font-semibold text-gray-900 dark:text-white">{source.name}</span>
+				</TableBodyCell>
+				<TableBodyCell>{source.priceString}</TableBodyCell>
+				<TableBodyCell>{source.dateString}</TableBodyCell>
+				<TableHeadCell>
+					<a href={source.source.productUrl} target="_blank" rel="noopener noreferrer">
+						<span class="sr-only">See</span>
+					</a>
+				</TableHeadCell>
+			</TableBodyRow>
+		{/each}
 	</TableBody>
 </Table>
